def7b506cf3e71162d46d0335ea9799a21469001
maryna-kryvoruchenko <kry20001@byui.edu>
2022-09-30 15:05:42 -0600

    changes

--- src/js/productDetails.js
+++ src/js/productDetails.js
@@ -7,9 +7,9 @@ export default class ProductDetails {
     }
     async init() {
         // use our datasource to get the details for the current product. findProductById will return a promise! use await or .then() to process it
-        
+        this.product = await this.dataSource.findProductById(this.productId);
         // once we have the product details we can render out the HTML
-        renderProductDetails();
+        document.querySelector('main').innerHTML = renderProductDetails();
         // once the HTML is rendered we can add a listener to Add to Cart button
         // Notice the .bind(this). Our callback will not work if we don't include that line. Review the readings from this week on 'this' to understand why.
         document.getElementById('addToCart').addEventListener('click', this.addToCart.bind(this));
@@ -18,7 +18,7 @@ export default class ProductDetails {
     addToCart() {
         setLocalStorage("so-cart", this.product);
     }
-    renderProductDetails(product) {
+    renderProductDetails() {
         return `
         <section class="product-detail">
         <h3>${this.product.Brand.Name}</h3>
